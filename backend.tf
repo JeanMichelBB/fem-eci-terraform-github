e7e2d0fb5402493ba0bb7e6d4a5a78a82fee738e
JeanMichelBB <jeanmichelbberube@outlook.com>
2024-12-20 09:27:11 -0500

    New message for the reverted commit, add gitignore

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  cloud {
+    organization = "seat-jmbberube"
+
+    workspaces {
+      name = "fem-eci-github"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "owner" {
+  default     = "JeanMichelBB"
+  type        = string
+}
