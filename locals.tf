6bf86313e5245fff4b9724de4be8f85348f896f9
JeanMichelBB <jeanmichelbberube@outlook.com>
2024-12-20 16:51:11 -0500

    feat: add service automation repo

--- locals.tf
+++ locals.tf
@@ -1,39 +1,46 @@
 locals {
-    repos = {
-        "fem-eci-terraform-tfe" = {
-            description         = "Automation for Terraform"
-            gitignore_template  = "Terraform"
-            name                = "fem-eci-terraform-tfe"
-            topics              = ["frontend-masters", "terraform"]
-            visibility          = "public"
-        }
-        "fem-eci-terraform-github" = {
-            description         = "Automation for GitHub"
-            gitignore_template  = "Terraform"
-            name                = "fem-eci-terraform-github"
-            topics              = ["frontend-masters", "terraform"]
-            visibility          = "public"
-        }
-        "fem-eci-terraform-aws-network" = {
-            description         = "Automation for GitHub"
-            gitignore_template  = "Terraform"
-            name                = "fem-eci-terraform-aws-network"
-            topics              = ["frontend-masters", "terraform"]
-            visibility          = "public"
-        }
-        "fem-eci-terraform-aws-cluster" = {
-            description         = "Automation for GitHub"
-            gitignore_template  = "Terraform"
-            name                = "fem-eci-terraform-aws-cluster"
-            topics              = ["frontend-masters", "terraform"]
-            visibility          = "public"
-        }
-        "fem-eci-service" = {
-            description         = "Example product service"
-            gitignore_template  = "Go"
-            name                = "fem-eci-service"
-            topics              = ["frontend-masters", "terraform"]
-            visibility          = "public"
-        }
+  repos = {
+    "fem-eci-terraform-tfe" = {
+      description        = "Automation for Terraform"
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-tfe"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
     }
+    "fem-eci-terraform-github" = {
+      description        = "Automation for GitHub"
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-github"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
+    }
+    "fem-eci-terraform-aws-network" = {
+      description        = "Automation for GitHub"
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-aws-network"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
+    }
+    "fem-eci-terraform-aws-cluster" = {
+      description        = "Automation for GitHub"
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-aws-cluster"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
+    }
+    "fem-eci-terraform-product-service" = {
+      description        = "Automation for product service"
+      gitignore_template = "Terraform"
+      name               = "fem-eci-terraform-product-service"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
+    }
+    "fem-eci-service" = {
+      description        = "Example product service"
+      gitignore_template = "Go"
+      name               = "fem-eci-service"
+      topics             = ["frontend-masters", "terraform"]
+      visibility         = "public"
+    }
+  }
 }
